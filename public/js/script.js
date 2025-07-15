32d0f60fd16a0a1180d07e8f549caa75eb7b329c
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-07-15 23:40:32 +0530

    Added the geolocation map

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,19 @@
 const socket=io();
+//searching the geolocation is available or not
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position)=>{
+        const {latitude, longitude}=position.coords;
+        socket.emit("send-location",{latitude,longitude});
+    },(error)=>{
+        console.error(error);
+        
+    },
+    {
+        enableHighAccuracy:true,
+        timeout:5000,
+        maximumAge:0,
+    }
+);
+}
 
+L.map("map").setView([0,0],10);
